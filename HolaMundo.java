6e4b6b2e19d83e65f31bc7cbec2a1741dd773711
Anthony Marcos Moroccoire Pacompia <amoroccoire@unsa.edu.pe>
2022-04-29 12:19:27 -0500

    se agregaron comentarios

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,8 +1,11 @@
 public class HolaMundo{
     public static void main(String args[]){
 
+        //Esto muestra Hola Mundo
         System.out.println("Hola Mundo");
+        //esto muestra mi nombre
         System.out.println("Anthony Marcos Moroccoire Pacompia");
+        //esto muestra un mensaje de prueba
         System.out.println("Esto es una prueba para GitHub");
     }
 }
